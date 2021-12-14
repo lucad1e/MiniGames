1df23a0a8da33e3004e040305e925e7f174d20f2
Luca <luca.dieling@gmail.com>
2021-12-15 00:53:26 +0100

    game

--- Hangman.js
+++ Hangman.js
@@ -3,16 +3,52 @@ const Discord = require("discord.js");
 const emojiList = require("./emojis.json")
 
 module.exports = async function(msg){
-    const guessedLetters = "";
-    const guessesRemaining = 6;
-    msg.channel.send("Your filthy mother").then(sentMsg => {
-        // sentMsg.react(emojiList.letters.a)
-        console.log(emojiList.letters.a)
-        for (let letter of Object.keys(emojiList.letters)){
-            // console.log(emojiList[letter])
-            sentMsg.react(emojiList.letters[letter])
-        }
-    }); 
-    //msg.react("")
+    game = new HangmanGame();
+    game.startGame(msg);
     return;
+}
+
+class HangmanGame{
+    constructor(){
+        this.gameEmbed = null;
+        this.word = "";
+        this.guessed = "";
+        this.guessesRemaining = 4;
+        this.inGame = false;
+    }
+
+    startGame(msg){
+        if (this.inGame){
+            return;
+        }
+        this.inGame = true;
+        this.word = "dog";
+        const embed = new Discord.MessageEmbed()
+            .setColor('#2ECC71')
+            .setTitle('Hangman')
+            .setDescription(this.getDescription())
+            .addField('Letters Guessed: ', 'A');
+    
+        
+
+        msg.channel.send({embeds: [embed]});
+        /*msg.channel.send({embeds: [embed]}).then(embSent =>{
+            this.gameEmbed = emsg;
+            this.waitForLetter()
+        });*/
+        return;
+    }
+    
+    getDescription(){
+        return  "```|‾‾‾‾‾| " +  
+            "\n|    " + (this.guessesRemaining < 4 ? "🎩" : " ") + 
+            "\n|    " + (this.guessesRemaining < 3 ? "😂" : " ") +
+            "\n|    " + (this.guessesRemaining < 2 ? "👕" : " ") +
+            "\n|    " + (this.guessesRemaining < 1 ? "👟" : " ") +
+            "\n|" + "\n|______" + "```"
+    }
+
+
+
+
 }
