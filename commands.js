8312a0c0b33cd849985dbf669a1c6b25f279c1d4
Luca <luca.dieling@gmail.com>
2021-12-17 00:21:12 +0100

    random Gods

--- commands.js
+++ commands.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch")
 const HangmanGame = require('./Hangman');
 const client = require("./MiniGames")
+const randomGod = require("./randomGods")
 
 module.exports = async function (msg){
     let tokens = msg.content.split(" ");
@@ -11,6 +12,18 @@ module.exports = async function (msg){
         //Hangman Handler
         console.log(`${msg.member.user.tag} is playing hangman`)
         HangmanGame(msg);
+    }
+    if(tokens[0].startsWith("!r")){
+        let amount = 1;
+        try{
+            amount = tokens.length > 1 ? parseInt(tokens[1]) : 1;
+        }
+        catch(error){
+            amount = 1;
+            console.log(error);
+        }
+        console.log(`${msg.member.user.tag} is rolling ${amount} god(s)`);
+        randomGod(amount);
     }
     return;
 }
