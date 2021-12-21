9ce4ceefc553935a37edcbb24a58624fe7599f24
Luca <luca.dieling@gmail.com>
2021-12-21 11:43:50 +0100

    dotenv

--- MiniGames.js
+++ MiniGames.js
@@ -1,11 +1,12 @@
 const commandHandler = require('./commands');
-const config = require("./settings.json");
+require('dotenv').config()
+
 const {Client, Intents} = require("discord.js");
 
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 module.exports = {client};
-client.login(config.Token);
+client.login(process.env.Token);
 client.on("ready", readyDiscord);
 
 function readyDiscord(){
